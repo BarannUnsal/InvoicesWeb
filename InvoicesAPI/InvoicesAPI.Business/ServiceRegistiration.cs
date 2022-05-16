be1468c466433e9c7334524104075f8583762f9a
Baran Unsal <barannunsal@gmail.com>
2022-05-16 21:37:06 +0300

    ServiceReg

--- InvoicesAPI/InvoicesAPI.Business/ServiceRegistiration.cs
+++ InvoicesAPI/InvoicesAPI.Business/ServiceRegistiration.cs
@@ -18,17 +18,5 @@ namespace InvoicesAPI.Business
         {
             services.AddScoped<IFileService, FileService>();
         }
-        public static void AddDALRegistration(this IServiceCollection services)
-        {
-
-            services.AddScoped<ICreditCardReadRepository, CreditCardReadRepository>();
-            services.AddScoped<ICreditCardWriteRepository, CreditCartWriteRepository>();
-            services.AddScoped<IHouseReadRepository, HouseReadRepository>();
-            services.AddScoped<IHouseWriteRepository, HouseWriteRepository>();
-            services.AddScoped<IInvoicesReadRepository, InvoiceReadRepository>();
-            services.AddScoped<IInvociesWriteRepository, InvoiceWriteRepository>();
-            services.AddScoped<IUserReadRepository, UserReadRepository>();
-            services.AddScoped<IUserWriteRepository, UserWriteRepository>();
-        }
     }
 }
