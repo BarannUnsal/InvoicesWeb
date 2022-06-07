3832140508d313cbf1b8730808f975944f7277a9
BarannUnsal <96000792+BarannUnsal@users.noreply.github.com>
2022-06-07 21:34:37 +0300

    Delete InvoicesClient directory

--- InvoicesClient/src/app/contracts/ListInvoice.ts
+++
@@ -1,9 +0,0 @@
-export class ListInvoice {
-    id: string;
-    invoiceNumber: Number;
-    invoiceType: string;
-    title: string;
-    description: string;
-    createdTime: Date;
-    updatedDate: Date;
-}
--- InvoicesClient/src/app/contracts/createinvoice.ts
+++
@@ -1,7 +0,0 @@
-export class CreateInvoice {
-    invoiceNumber: number;
-    invoiceType: string;
-    title: string;
-    description: string;
-    isActive: boolean = true;
-}
