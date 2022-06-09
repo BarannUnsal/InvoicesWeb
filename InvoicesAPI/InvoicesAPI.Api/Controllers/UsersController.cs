9f61a586007ff6ed8c917c031eeb4b0b3bd4d880
Baran Unsal <barannunsal@gmail.com>
2022-06-10 00:17:18 +0300

    Login User CQRS

--- InvoicesAPI/InvoicesAPI.Api/Controllers/UsersController.cs
+++ InvoicesAPI/InvoicesAPI.Api/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using InvoicesAPI.Business.Features.Command.AppUser.CreateUser;
+using InvoicesAPI.Business.Features.Command.AppUser.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -18,6 +19,13 @@ namespace InvoicesAPI.Api.Controllers
         public async Task<IActionResult> CreateUser(CreateUserCommandRequest request)
         {
             CreateUserCommandResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login (LoginUserCommandRequest request)
+        {
+            LoginUserCommandResponse response = await _mediator.Send(request);
             return Ok(response);
         }
     }
