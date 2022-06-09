fabeec8e4924e3c82c9c4132080c7897e856b12c
Baran Unsal <barannunsal@gmail.com>
2022-06-10 00:11:24 +0300

    Exceptions class added

---
+++ InvoicesAPI/InvoicesAPI.Business/Exceptions/Users/UserCreateFailedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InvoicesAPI.Business.Exceptions.Users
+{
+    public class UserCreateFailedException : Exception
+    {
+        public UserCreateFailedException() : base("Kullanıcı oluşturulurken beklenmeyen hata!")
+        {
+        }
+
+        public UserCreateFailedException(string message) : base(message)
+        {
+        }
+
+        public UserCreateFailedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
