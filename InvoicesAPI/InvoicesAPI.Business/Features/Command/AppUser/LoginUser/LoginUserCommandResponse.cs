5053e2c9414ddb501279746d2923d3ab62fc6d5b
Baran Unsal <barannunsal@gmail.com>
2022-06-13 17:24:04 +0300

    JWT operation

---
+++ InvoicesAPI/InvoicesAPI.Business/Features/Command/AppUser/LoginUser/LoginUserCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using InvoicesAPI.Business.DTOs;
+
+namespace InvoicesAPI.Business.Features.Command.AppUser.LoginUser
+{
+    public class LoginUserCommandResponse
+    {
+    }
+    public class LoginUserCommandSuccessRespone : LoginUserCommandResponse
+    {
+        public Token Token { get; set; }
+    }
+    public class LoginUserCommandErrorResponse : LoginUserCommandResponse
+    {
+        public string Message { get; set; }
+    }
+}
