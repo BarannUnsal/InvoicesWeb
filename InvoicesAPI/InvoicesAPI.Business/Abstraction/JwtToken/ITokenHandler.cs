522f62611ba667de202c247ba5dea5bfaa02d0e8
Baran Unsal <barannunsal@gmail.com>
2022-06-13 17:24:57 +0300

    JWT operation

---
+++ InvoicesAPI/InvoicesAPI.Business/Abstraction/JwtToken/ITokenHandler.cs
@@ -0,0 +1,9 @@
+﻿using InvoicesAPI.Business.DTOs;
+
+namespace InvoicesAPI.Business.Abstraction.JwtToken
+{
+    public interface ITokenHandler 
+    {
+        Token CreateAccessToken(int minute);
+    }
+}
