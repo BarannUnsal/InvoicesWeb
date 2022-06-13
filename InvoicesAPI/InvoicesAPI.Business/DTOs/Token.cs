50d21e9be20d2bc000e8a7feb827af0ade9d6e62
Baran Unsal <barannunsal@gmail.com>
2022-06-13 17:21:37 +0300

    DTO obj created and jtw token obj created

---
+++ InvoicesAPI/InvoicesAPI.Business/DTOs/Token.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace InvoicesAPI.Business.DTOs
+{
+    public class Token
+    {
+        public string AccessToken { get; set; }
+        public DateTime Expiration { get; set; } 
+    }
+}
